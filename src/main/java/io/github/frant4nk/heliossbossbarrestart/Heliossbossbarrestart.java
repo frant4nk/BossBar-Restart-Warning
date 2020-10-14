4369ce487b59a6b0baf2d75e75152f0bf41863e6
frant4nk <72709186+frant4nk@users.noreply.github.com>
2020-10-14 13:57:30 +0200

    sync 1.0.1

--- src/main/java/io/github/frant4nk/heliossbossbarrestart/Heliossbossbarrestart.java
+++ src/main/java/io/github/frant4nk/heliossbossbarrestart/Heliossbossbarrestart.java
@@ -84,7 +84,7 @@ public class Heliossbossbarrestart {
 
     public static int getTimeLeft()
     {
-        int[] timesArray = new int[]{17940, 39540, 61140, 82740, 43140}; //04:58:50, 10:58:50, 16:58:50, 22:58:50, TEST
+        int[] timesArray = new int[]{17940, 39540, 61140, 82740, 43740}; //04:58:50, 10:58:50, 16:58:50, 22:58:50, TEST
 
         LocalDateTime localDate = LocalDateTime.now();
         int hours = localDate.getHour();
